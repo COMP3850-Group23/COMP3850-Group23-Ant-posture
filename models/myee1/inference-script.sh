0677b395a21e7a269c72c1747e0d4032613ac517
Michael Yee <michael.yee@students.mq.edu.au>
2024-04-16 09:52:13 +1000

    Add initial models

---
+++ models/myee1/inference-script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sleap-track D:\Documents\COMP3850-Group23-Ant-posture\datasets\main.v001.slp --only-suggested-frames -m models\240409_214243.centroid -m models\240409_214243.centered_instance -o main.v001.slp.predictions.slp --verbosity json --no-empty-frames

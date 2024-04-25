a37f2d79d17168187936bcf129cc8fad33f1b176
Michael Yee <michael.yee@students.mq.edu.au>
2024-04-25 15:18:56 +1000

    Remove obsolete models

--- models/myee1/inference-script.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sleap-track D:\Documents\COMP3850-Group23-Ant-posture\datasets\main.v001.slp --only-suggested-frames -m models\240409_214243.centroid -m models\240409_214243.centered_instance -o main.v001.slp.predictions.slp --verbosity json --no-empty-frames
